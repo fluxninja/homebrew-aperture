ecd5fcedeab12afa0bf067e34621c49f42b5d057
Hardik Shingala <34568645+hdkshingala@users.noreply.github.com>
2023-02-27 18:20:55 +0530

    Release v0.25.0-rc.2 (#11)

--- Formula/aperturectl@0.25.rb
+++ Formula/aperturectl@0.25.rb
@@ -1,8 +1,8 @@
 class AperturectlAT025 < Formula
   desc "CLI for flow control and reliability management for modern web applications"
   homepage "https://www.fluxninja.com"
-  url "https://github.com/fluxninja/aperture/archive/v0.25.0-rc.1.tar.gz"
-  sha256 "106dd6119ce413593f68a2420e462cf56ccff19797215d53e61381b78d48881b"
+  url "https://github.com/fluxninja/aperture/archive/v0.25.0-rc.2.tar.gz"
+  sha256 "fa122d5d05575717a32107c99bbd8e5a7a438bf413cf3042dafb5d1fb9bba7ce"
   license "Apache-2.0"
   head "https://github.com/fluxninja/aperture.git", branch: "stable/v0.25.x"
 
@@ -12,7 +12,7 @@ class AperturectlAT025 < Formula
 
   def install
     ENV["GIT_BRANCH"]="stable/v0.25.x"
-    ENV["GIT_COMMIT_HASH"]="e3203be0f77ad28ce419cc1838dcb9a47213ee35"
+    ENV["GIT_COMMIT_HASH"]="d35e4f6843b0fb05768883c54dc22608ad419248"
     ENV["SOURCE"]="./cmd/aperturectl"
     ENV["TARGET"]=bin/"aperturectl"
     ENV["VERSION"]=version
