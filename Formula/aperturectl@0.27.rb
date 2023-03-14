04ec51871fb8e62c75b8a01ce5ab01c284b84d78
CircleCI-Gitbot <circleci-gitbot@fluxninja.com>
2023-03-14 09:49:54 +0000

    Update brews for release v0.27.0

--- Formula/aperturectl@0.27.rb
+++ Formula/aperturectl@0.27.rb
@@ -1,8 +1,8 @@
 class AperturectlAT027 < Formula
   desc "CLI for flow control and reliability management for modern web applications"
   homepage "https://www.fluxninja.com"
-  url "https://github.com/fluxninja/aperture/archive/v0.27.0-rc.1.tar.gz"
-  sha256 "86285a77c8b241aacc6d2b070dde3a04ddcf08570cd41f09d22b4f499f52bdb5"
+  url "https://github.com/fluxninja/aperture/archive/v0.27.0.tar.gz"
+  sha256 "3a6f37d5b1e89b48cf99e8ba8b41949941a0530b410a888a2ecf789869ade761"
   license "Apache-2.0"
   head "https://github.com/fluxninja/aperture.git", branch: "stable/v0.27.x"
 
@@ -12,7 +12,7 @@ class AperturectlAT027 < Formula
 
   def install
     ENV["GIT_BRANCH"]="stable/v0.27.x"
-    ENV["GIT_COMMIT_HASH"]="ef20ce792b511bd2f3fd71afa108698554d480b4"
+    ENV["GIT_COMMIT_HASH"]="34908723776963a9de9d88aff476da50831a3e5d"
     ENV["SOURCE"]="./cmd/aperturectl"
     ENV["TARGET"]=bin/"aperturectl"
     ENV["VERSION"]=version
