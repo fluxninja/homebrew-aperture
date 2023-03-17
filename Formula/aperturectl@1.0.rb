819e0590ed3d8ec668c236dde9ccfefa116fb5fd
CircleCI-Gitbot <circleci-gitbot@fluxninja.com>
2023-03-17 16:17:49 +0000

    Update brews for release v1.0.0-rc.1

--- Formula/aperturectl@1.0.rb
+++ Formula/aperturectl@1.0.rb
@@ -1,25 +1,25 @@
-class AperturectlAT025 < Formula
+class AperturectlAT10 < Formula
   desc "CLI for flow control and reliability management for modern web applications"
   homepage "https://www.fluxninja.com"
-  url "https://github.com/fluxninja/aperture/archive/v0.25.2.tar.gz"
-  sha256 "f4ed12a9b98f42cd2d01224e85f517ee422bd93d46991e0c33c45898dddad029"
+  url "https://github.com/fluxninja/aperture/archive/v1.0.0-rc.1.tar.gz"
+  sha256 "7b06deddf6796edbefde1c4448bbd143e3f7bfb9cbbf22aee47bb3a5cc4533c2"
   license "Apache-2.0"
-  head "https://github.com/fluxninja/aperture.git", branch: "stable/v0.25.x"
+  head "https://github.com/fluxninja/aperture.git", branch: "stable/v1.0.x"
 
   keg_only :versioned_formula
 
   depends_on "go" => :build
 
   def install
-    ENV["GIT_BRANCH"]="stable/v0.25.x"
-    ENV["GIT_COMMIT_HASH"]="651416e96dba6a8c78c0af1e28ad3a6f5c9cc342"
+    ENV["GIT_BRANCH"]="stable/v1.0.x"
+    ENV["GIT_COMMIT_HASH"]="210c75c49e6737e82050e85e65e634c57820f1a1"
     ENV["SOURCE"]="./cmd/aperturectl"
     ENV["TARGET"]=bin/"aperturectl"
     ENV["VERSION"]=version
     ENV["PREFIX"]="aperture"
     ENV["LDFLAGS"]="-s -w"
     if build.head?
-      head_branch="stable/v0.25.x"
+      head_branch="stable/v1.0.x"
       require "open3"
       stdout, status = Open3.capture2("git", "log", "-n1", "--format=%H")
       odie "Unable to get commit hash for head build" if status != 0
