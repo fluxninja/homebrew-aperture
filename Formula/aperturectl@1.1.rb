9c856717b1075a40e662bb6ee7169a61b1fb7fef
FluxNinja Ops <111978908+fluxninjaops@users.noreply.github.com>
2023-03-24 18:45:30 +0530

    Update brews for release v1.1.0-rc.1 (#29)
    Co-authored-by: CircleCI-Gitbot <circleci-gitbot@fluxninja.com>

---
+++ Formula/aperturectl@1.1.rb
@@ -0,0 +1,43 @@
+class AperturectlAT11 < Formula
+  desc "CLI for flow control and reliability management for modern web applications"
+  homepage "https://www.fluxninja.com"
+  url "https://github.com/fluxninja/aperture/archive/v1.1.0-rc.1.tar.gz"
+  sha256 "08f7899098f9d8ecaaf9fb4044fe4c308269e5c4b32aa0fa815245f3ffc37ed3"
+  license "Apache-2.0"
+  head "https://github.com/fluxninja/aperture.git", branch: "stable/v1.1.x"
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  def install
+    git_branch="stable/v1.1.x"
+    git_commit_hash="89738169dad8049ab8ad102b5d2fb8b921103017"
+
+    require "open3"
+    if build.head?
+      head_branch="stable/v1.1.x"
+      stdout, status = Open3.capture2("git", "log", "-n1", "--format=%H")
+      odie "Unable to get commit hash for head build" if status != 0
+      git_commit_hash=stdout
+      git_branch=head_branch
+    end
+
+    ENV["APERTURECTL_BUILD_VERSION"]=version
+    ENV["APERTURECTL_BUILD_GIT_BRANCH"]=git_branch
+    ENV["APERTURECTL_BUILD_GIT_COMMIT_HASH"]=git_commit_hash
+
+    stdout, status = Open3.capture2("./scripts/build_aperturectl.sh", buildpath/"cmd/aperturectl")
+    odie "Failed to build aperturectl" if status != 0
+
+    # Move the binary into the final location
+    makedirs bin
+    mv stdout, bin/"aperturectl"
+
+    generate_completions_from_executable(bin/"aperturectl", "completion")
+  end
+
+  test do
+    assert_match "aperturectl version #{version}", shell_output("#{bin}/aperturectl --version")
+  end
+end
