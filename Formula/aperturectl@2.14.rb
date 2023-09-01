4320f904825d6c375416b45ac323c0e2734c77c3
CircleCI-Gitbot <circleci-gitbot@fluxninja.com>
2023-09-01 12:49:06 +0000

    Update brews for release v2.14.0-rc.1

--- Formula/aperturectl@2.14.rb
+++ Formula/aperturectl@2.14.rb
@@ -1,22 +1,22 @@
-class AperturectlAT211 < Formula
+class AperturectlAT214 < Formula
   desc "CLI for flow control and reliability management for modern web applications"
   homepage "https://www.fluxninja.com"
-  url "https://github.com/fluxninja/aperture/archive/v2.11.1.tar.gz"
-  sha256 "ea3ca45baaab44a92b4dd78bcdec85313392ab6ba96bc77b7679f9c6e96b58ca"
+  url "https://github.com/fluxninja/aperture/archive/v2.14.0-rc.1.tar.gz"
+  sha256 "a89a4a0a46b3378b552d1837eaabb6bc87a495284b11d8443c4865cc0a145cba"
   license "Apache-2.0"
-  head "https://github.com/fluxninja/aperture.git", branch: "stable/v2.11.x"
+  head "https://github.com/fluxninja/aperture.git", branch: "stable/v2.14.x"
 
   keg_only :versioned_formula
 
   depends_on "go" => :build
 
   def install
-    git_branch="stable/v2.11.x"
-    git_commit_hash="6c1253c0257aa9bdbb978250757d4e7c1af740db"
+    git_branch="stable/v2.14.x"
+    git_commit_hash="c6c6b5e7539cc4233fdccfc737513e8332fc57f5"
 
     require "open3"
     if build.head?
-      head_branch="stable/v2.11.x"
+      head_branch="stable/v2.14.x"
       stdout, status = Open3.capture2("git", "log", "-n1", "--format=%H")
       odie "Unable to get commit hash for head build" if status != 0
       git_commit_hash=stdout
