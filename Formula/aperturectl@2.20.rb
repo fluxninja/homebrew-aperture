3f684c4c67bc345a4994215dbd4da8b817c852af
CircleCI-Gitbot <circleci-gitbot@fluxninja.com>
2023-10-16 13:10:17 +0000

    Update brews for release v2.20.0-rc.1

--- Formula/aperturectl@2.20.rb
+++ Formula/aperturectl@2.20.rb
@@ -1,22 +1,28 @@
-class AperturectlAT217 < Formula
+class AperturectlAT220 < Formula
   desc "CLI for flow control and reliability management for modern web applications"
   homepage "https://www.fluxninja.com"
-  url "https://github.com/fluxninja/aperture/archive/v2.17.0.tar.gz"
-  sha256 "3b1849bff77e3cd78a14c3278effce9c6dbeb3d23ed619b9303b5cbf987f2765"
+  url "https://github.com/fluxninja/aperture/archive/v2.20.0-rc.1.tar.gz"
+  sha256 "d85d1c2e809946873aad7981e8e1563839547b57a65ffb04ecf135452f8e5eb1"
   license "Apache-2.0"
-  head "https://github.com/fluxninja/aperture.git", branch: "stable/v2.17.x"
+  head "https://github.com/fluxninja/aperture.git", branch: "stable/v2.20.x"
 
   keg_only :versioned_formula
+
+  bottle do
+    root_url "https://github.com/fluxninja/homebrew-aperture/releases/download/aperturectl-2.19.4"
+    sha256 cellar: :any_skip_relocation, monterey:     "dc42abd32d3191e0801cd0ede9ed691ed291716fdd0b672f0a454905c7337a84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "02a5b85dd2445ba8807008e7fc774c971e18f7021d69420e7802f6b5ce6b69e7"
+  end
 
   depends_on "go" => :build
 
   def install
-    git_branch="stable/v2.17.x"
-    git_commit_hash="bfb90c66e03fc0827740b8d0f36dc58f3ebf0a69"
+    git_branch="stable/v2.20.x"
+    git_commit_hash="13ed5a2edfa4e01f6b25dd3302778f40ff211173"
 
     require "open3"
     if build.head?
-      head_branch="stable/v2.17.x"
+      head_branch="stable/v2.20.x"
       stdout, status = Open3.capture2("git", "log", "-n1", "--format=%H")
       odie "Unable to get commit hash for head build" if status != 0
       git_commit_hash=stdout
