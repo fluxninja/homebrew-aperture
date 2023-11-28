8169f290bee1f9675d9c871944b46c916c85a127
CircleCI-Gitbot <circleci-gitbot@fluxninja.com>
2023-11-28 07:09:02 +0000

    Update brews for release v2.25.0-rc.1
    Closes #189.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/aperturectl@2.25.rb
+++ Formula/aperturectl@2.25.rb
@@ -1,16 +1,15 @@
-class AperturectlAT222 < Formula
+class AperturectlAT225 < Formula
   desc "CLI for flow control and reliability management for modern web applications"
   homepage "https://www.fluxninja.com"
-  url "https://github.com/fluxninja/aperture/archive/refs/tags/v2.22.1.tar.gz"
-  sha256 "25d69625f53e9aeaee1f87a96f67b2c879d3a6efb0b16d04781cc43bfc4320a9"
+  url "https://github.com/fluxninja/aperture/archive/refs/tags/v2.25.0-rc.1.tar.gz"
+  sha256 "50a025e97bf5e07c0cdd76e091de2b89d6778fb2bdf1a3d845a2a5440f0a7fb6"
   license "Apache-2.0"
   head "https://github.com/fluxninja/aperture.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/fluxninja/homebrew-aperture/releases/download/aperturectl@2.22-2.22.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, monterey:     "0d3102a8915bac130ac2a6615ad6475c9c3e873b2a8a2fa6a12c35eb87891675"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ffe2181c8a318cac7e6b740521a55f8041fa2e7ad97dda21d2aa91c47031d39a"
+    root_url "https://github.com/fluxninja/homebrew-aperture/releases/download/aperturectl-2.24.1"
+    sha256 cellar: :any_skip_relocation, monterey:     "637327d609aa142f8d58886b4c644590a25cc284470fd4b4a8f801b507464180"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7586905e52bd30b32567c78cff31ab9355ff1fad47e17b25f49b37ae40489c41"
   end
 
   keg_only :versioned_formula
@@ -18,12 +17,12 @@ class AperturectlAT222 < Formula
   depends_on "go" => :build
 
   def install
-    git_branch="stable/v2.22.x"
-    git_commit_hash="4fbd606098c604389a30d29b5322b73f2f47d734"
+    git_branch="stable/v2.25.x"
+    git_commit_hash="4816260a01cac182443ef076e4776cba0c943c78"
 
     require "open3"
     if build.head?
-      head_branch="stable/v2.22.x"
+      head_branch="stable/v2.25.x"
       stdout, status = Open3.capture2("git", "log", "-n1", "--format=%H")
       odie "Unable to get commit hash for head build" if status != 0
       git_commit_hash=stdout
